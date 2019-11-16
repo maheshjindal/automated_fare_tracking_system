3f137e1fa7d3da8cd0dc80fe7f87aa2d3f88f593
maheshjindal <mahesh.jindal6@gmail.com>
2019-11-16 14:47:39 +0530

    Added Firebase Integration

--- src/main/java/com/pirates/frts/rest/TravelController.java
+++ src/main/java/com/pirates/frts/rest/TravelController.java
@@ -2,7 +2,9 @@ package com.pirates.frts.rest;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TravelController {
+
 }
